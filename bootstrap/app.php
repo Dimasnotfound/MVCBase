e5ac45a8d1db985687e10b12434e2d26c951d5d9
Dimasnotfound <dp4369344@gmail.com>
2025-02-02 20:48:06 +0700

    fix $app not detected in index.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,4 +2,8 @@
 
 require __DIR__.'/../config/database.php';
 
+$app = [
+    'config' => require __DIR__.'/../config/database.php'
+];
+
 return $app;
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
-require __DIR__.'/../bootstrap/app.php';
+$app = require __DIR__.'/../bootstrap/app.php';
 
-echo 'Welcome to PHP MVC Project!';
+echo 'Welcome to PHP MVC Project!<br>';
+echo 'Database Host: ' . $app['config']['host'];
