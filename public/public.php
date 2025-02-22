729868c1137a33410640de4ae09033e92dda72ea
Dimasnotfound <dp4369344@gmail.com>
2025-02-22 12:05:35 +0700

    add:edit public

--- public/public.php
+++ public/public.php
@@ -2,10 +2,12 @@
 session_start();
 
 spl_autoload_register(function ($class) {
-    if (file_exists('../app/core/' . $class . '.php')) {
-        require_once '../app/core/' . $class . '.php';
-    } elseif (file_exists('../app/controllers/' . $class . '.php')) {
-        require_once '../app/controllers/' . $class . '.php';
+    if (file_exists("../app/core/{$class}.php")) {
+        require_once "../app/core/{$class}.php";
+    } elseif (file_exists("../app/controllers/{$class}.php")) {
+        require_once "../app/controllers/{$class}.php";
+    } elseif (file_exists("../app/models/{$class}.php")) {
+        require_once "../app/models/{$class}.php";
     }
 });
 
